89a14befc73f48a9115d012f94cdcce2fabea68a
shubhangi1603 <shubhangimishra1603@gmail.com>
2021-10-12 23:33:45 +0530

    ....

--- app.py
+++ app.py
@@ -1,76 +1,94 @@
+from ctypes import alignment
+from os import write
+from re import template
 import streamlit as st
 import plotly.graph_objs as go
 from preparedata import loadData
 import plotly.express as px
 
 df = loadData('most_followed_ig.csv')
-
-st.title('ANALAYSIS OF POPULAR CELEBRITIES')
 sidebar= st.sidebar
+st.sidebar.image('icon.png')
 sidebar.header('Choose your option')
-choices =['Project overview','Analyses']
-selOpt =sidebar.selectbox('choose option you want',choices)
-st.image('logo.jpeg')
+choices =["ProjectOverview", " facts ","analyseData"]
+selOpt =sidebar.selectbox('choose here',choices)
 
-def analyseData():
-    st.header('visulization here')
-    if selOpt == 'Project overview':
+def ProjectOverview():
+    st.title('ANALYSES OF POPULAR INSTAGRAMERS')
+    st.image('logo.jpeg')
+    st.markdown('''
+    ## WELCOME TO MY DATA ANALYTICS  PROJECT.
+    
+    THE MOTIVE OF THIS IS TO ANALYSE THE MOST POPULAR INSTAGRAM CELEBRITY ON THE BASIS OF NUMBERS OF FOLLOWERS THEY HAVE
+    ''',unsafe_allow_html=True)
 
-        analyseData()
+def facts():
+     st.title(" SOME FACTS ABOUT PROJECT")
+     st.image('instagram.jpg')
+     st.markdown('''
 
-    elif selOpt == 'Analyses':
-
-        analyseData()   
-
-
-
-def init():
     
 
-    st.markdown("""
-    ### Perameters of analysis
-1. On the bases of numbers of followers
-2. Most ranked
-3. Categories wise
-4. Number of posts made
-
-**Mentioned parameters will give the better understanding of project.
-  
-
-""",unsafe_allow_html=True)
-st.write('')
-st.markdown("""
-### Analyse of most trending clebrities of instagram
-    How we gonna analyse the popularity of each celebrity ?
-    : We have some parameters for anlysing this
-
-
-""",unsafe_allow_html=True)
+      The sum of all their followers is a whopping 6.14070613 users.
+     That number is misleadingly large however, because if I follow both @instagram and @beyonce, 
+     I would be counted twice in there. Still, it’s a big number.
+    The total number of posts in this data set is 315,888
     
-init()
+    ** Parameters used to analyse the popularity of celebreties  -- FOLLOWERS and ENGAGEMENT RATE**
+
+
+    1.IN THIS PROJECT THE MAIN ASPECT OF ANALYZING THE POPULARITIES OF CELEBS ARE THE FOLLOWERS.AND "SELANA GOMEZ" TOP IN THIS PARAMETER.
+
+    2.SECOND IMPORTANT ASPECT IS THE NUMBER OF POST EACH INSTAGRAMER OF THIS DATASET HAS MADE.
+
+    3.LAST BUT IMPORTANT ASPECT IS THE 'ENGAGEMENT RATE OF EACH POST'
+
+    
+    
+    
+    ''', unsafe_allow_html=True)
+
+
+    
+    
+
+    
+               
+    
+
+
 
 def analyseData():
-    st.subheader('MOST FOLLOWED IG CELEBES BY RANK')
-    st.plotly_chart(px.histogram(data_frame = df, x ='BRAND', y ='RANK', color='FOLLOWERS',template='plotly_white' ))
-    st.plotly_chart( px.bar(data_frame= df ,  x='RANK', y='BRAND',
-             hover_data=['MEDIA POSTED', 'FOLLOWERS'], color='RANK',
-             labels={'BRAND':'BRAND'}, template='seaborn'))
-    st.plotly_chart(px.bar(data_frame= df, x='BRAND', y ='CATEGORIES 1', color='FOLLOWERS',
-                    template='seaborn'))
-    st.plotly_chart(px.bar(data_frame= df ,  x='BRAND', y=  'CATEGORIES 2',
-             hover_data=['MEDIA POSTED', 'ER'], color='FOLLOWERS' ,
-             labels={'FOLLOWERS':'FOLLOWERS'}))
-    st.plotly_chart(px.scatter_3d(data_frame= df, x ='BRAND' ,y= 'CATEGORIES 1', z = 'CATEGORIES 2' ,color='iPOSTS ON HASHTAG' ,height= 700,   color_continuous_scale=px.colors.convert_colorscale_to_rgb  ,template= 'plotly_white'))         
-    st.plotly_chart(px.scatter(df, x="BRAND", y="CATEGORIES 1", color='FOLLOWERS', template='ggplot2'))
-    st.plotly_chart(px.bar(data_frame = df, x ='BRAND', y ='CATEGORIES 2' ,color= 'MEDIA POSTED' , template='seaborn'))  
-    st.plotly_chart(px.bar(data_frame = df, x ='BRAND', y ='CATEGORIES 1' ,color= 'iPOSTS ON HASHTAG' , template='seaborn'))  
-    st.plotly_chart(px.scatter(df, x="BRAND", y="CATEGORIES 2", color='MEDIA POSTED', template='seaborn')) 
-    st.plotly_chart(px.bar(df, x="BRAND", y="CATEGORIES 2", color="RANK",
-             hover_data=["ER", "FOLLOWERS"],
-             height=400,template='seaborn')
-             )              
+    df['FOLLOWERS']= df['FOLLOWERS'].apply(lambda a: float(a.split('M')[0]))
+    df['FOLLOWERS'].head()
+    st.subheader('ANALYSES BY FOLLOWERS')
+    st.plotly_chart((px.bar(data_frame = df, y='FOLLOWERS', x='BRAND',   color='RANK')))
+    
+    
 
-analyseData()             
+
+
+
+
+if selOpt == choices[0]:
+    ProjectOverview()
+
+elif selOpt ==choices[1]: 
+    facts()
+
+elif selOpt == choices[2]:
+    analyseData()       
+
+    
+
+
+
+    
+
+
+
+
+
            
 
    
